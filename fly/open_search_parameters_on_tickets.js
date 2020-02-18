19d56b24c328af02e5b0d4dcc13406c45d5665d2
Helen Smirnova <helensmirnova1992@gmail.com>
2020-02-18 22:20:20 +0200

    Update open_search_parameters_on_tickets.js

--- fly/open_search_parameters_on_tickets.js
+++ fly/open_search_parameters_on_tickets.js
@@ -29,7 +29,7 @@
 function doo(){	
   var modifyBtn = document.getElementsByClassName('modify-search')[0];
   modifyBtn.click();
-  var modalBg = document.getElementsByClassName('cdk-overlay-backdrop mat-dialog-backdrop-light cdk-overlay-backdrop-showing')[0];
+  var modalBg = document.getElementsByClassName('mat-dialog-backdrop-light')[0];
   modalBg.style.visibility = 'hidden';
 }
 
