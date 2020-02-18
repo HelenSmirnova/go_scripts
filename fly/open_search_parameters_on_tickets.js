a094db6a1a58ad913ac8fe8502518db2c565255a
Helen Smirnova <helensmirnova1992@gmail.com>
2020-02-18 22:28:44 +0200

    Update open_search_parameters_on_tickets.js

--- fly/open_search_parameters_on_tickets.js
+++ fly/open_search_parameters_on_tickets.js
@@ -1,7 +1,7 @@
 function doo(){	
   var modifyBtn = document.getElementsByClassName('obe-login')[0];
   modifyBtn.click();
-  var modalBg = document.getElementsByClassName('mat-dialog-backdrop-light')[0];
+  var modalBg = document.getElementsByClassName('mat-dialog-backdrop-light cdk-overlay-backdrop-showing')[0];
   modalBg.style.visibility = 'hidden';
 }
 
