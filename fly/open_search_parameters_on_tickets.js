a03704edacc547a594f293b2524eb7d5cf590a5a
Helen Smirnova <helensmirnova1992@gmail.com>
2020-02-18 22:16:51 +0200

    Update open_search_parameters_on_tickets.js

--- fly/open_search_parameters_on_tickets.js
+++ fly/open_search_parameters_on_tickets.js
@@ -1,4 +1,36 @@
-var modifyBtn = document.getElementsByClassName('modify-search')[0];
-modifyBtn.click();
-var modalBg = document.getElementsByClassName('cdk-overlay-backdrop mat-dialog-backdrop-light cdk-overlay-backdrop-showing')[0];
-modalBg.style.visibility = 'hidden';
+// function initjQuery(callback) {
+// 	var script = document.createElement( "script" )
+// 	script.type = "text/javascript";
+// 	if(script.readyState) {
+// 	script.onreadystatechange = function() {
+// 		if ( script.readyState === "loaded" || script.readyState === "complete" ) {
+// 			script.onreadystatechange = null;
+// 			callback();
+// 		}
+// 	};
+//   } else {
+// 	script.onload = function() {
+// 		callback();
+// 	};
+//   }
+//   script.src = '//code.jquery.com/jquery-3.3.1.min.js';
+//   document.getElementsByTagName( "head" )[0].appendChild( script );
+// }
+// if (!window.jQuery) {
+// 	initjQuery(function() {
+// 		readyjQueryinit();
+// 	});
+// }else{
+// 	readyjQueryinit();
+// }
+
+
+
+function doo(){	
+  var modifyBtn = document.getElementsByClassName('modify-search')[0];
+  modifyBtn.click();
+  var modalBg = document.getElementsByClassName('cdk-overlay-backdrop mat-dialog-backdrop-light cdk-overlay-backdrop-showing')[0];
+  modalBg.style.visibility = 'hidden';
+}
+
+
