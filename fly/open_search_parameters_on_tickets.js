f6bd5920320efd15af4109d4dbfd4724decdecba
Olena Smirnova <helensmirnova1992@gmail.com>
2020-02-18 22:11:41 +0200

    init

---
+++ fly/open_search_parameters_on_tickets.js
@@ -0,0 +1,4 @@
+var modifyBtn = document.getElementsByClassName('modify-search')[0];
+modifyBtn.click();
+var modalBg = document.getElementsByClassName('cdk-overlay-backdrop mat-dialog-backdrop-light cdk-overlay-backdrop-showing')[0];
+modalBg.style.visibility = 'hidden';
