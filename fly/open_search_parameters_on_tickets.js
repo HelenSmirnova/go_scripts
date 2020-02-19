a1ee6204fb97c87230fd63c4b1737b9a852c0dae
Olena Smirnova <helensmirnova1992@gmail.com>
2020-02-19 08:25:44 +0200

    .

--- fly/open_search_parameters_on_tickets.js
+++ fly/open_search_parameters_on_tickets.js
@@ -1,36 +1,38 @@
 function initjQuery(callback) {
-	var script = document.createElement( "script" )
-	script.type = "text/javascript";
-	if(script.readyState) {
-	script.onreadystatechange = function() {
-		if ( script.readyState === "loaded" || script.readyState === "complete" ) {
-			script.onreadystatechange = null;
-			callback();
-		}
-	};
-  } else {
-	script.onload = function() {
-		callback();
-	};
-  }
-  script.src = '//code.jquery.com/jquery-3.3.1.min.js';
-  document.getElementsByTagName( "head" )[0].appendChild( script );
+    var script = document.createElement("script")
+    script.type = "text/javascript";
+    if (script.readyState) {
+        script.onreadystatechange = function () {
+            if (script.readyState === "loaded" || script.readyState === "complete") {
+                script.onreadystatechange = null;
+                callback();
+            }
+        };
+    } else {
+        script.onload = function () {
+            callback();
+        };
+    }
+    script.src = '//code.jquery.com/jquery-3.3.1.min.js';
+    document.getElementsByTagName("head")[0].appendChild(script);
 }
 if (!window.jQuery) {
-	initjQuery(function() {
-		readyjQueryinit();
-	});
-}else{
-	readyjQueryinit();
+    initjQuery(function () {
+        readyjQueryinit();
+    });
+} else {
+    readyjQueryinit();
 }
 
 
 
-function readyjQueryinit(){
- $( ".search-summary" ).load(function() {
-   $(".way-oneway").click();
-});
-	
+function readyjQueryinit() {
+    setInterval(function () {
+        $(".search-summary").load(function () {
+            $(".way-oneway").click();
+        });
+    }, 300);
+
 }
 
 
